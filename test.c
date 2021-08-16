b73fc88f543eaafa04b4a9b005261a855ad751ec
daimalvtu <88648349+daimalvtu@users.noreply.github.com>
2021-08-16 22:38:44 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,39 +1,14 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 #include<string.h>
-//int main()
-//{
-//	//char arr[] = { 'a','b','c','d' };
-//	////int len = strlen(arr);
-//	char arr[6] = "asdfg";
-//	printf("%d\n ", strlen(arr));
-//	/*int arr[][4] = { {1,2,3},{1,2,4,5} };
-//	int i = 0;
-//	for ( i = 0; i < 4; i++)
-//	{
-//		int j = 0;
-//		for ( j = 0; j < 4; j++)
-//		{
-//			printf("%d ",arr[i][j]);
-//		}
-//		printf("\n");
-//	}*/
-//	return 0;
-//}
-//只要是循环语句就可以用break,还有开关语句Switch中也可以使用
-//int main()
-//{
-//	int i = 1;
-//	while (i != 7)
-//	{
-//		i += 2;
-//		break;
-//	}
-//	printf("%d\n",i);
-//	return 0;
-//}
 int main()
 {
-	printf("hhh");
+	/*char arr[5] = "qwerr";
+	int len = strlen(arr);
+	printf("%d\n",len);*/
+	int arr[] = {'a','b','c','d'};
+	int len = strlen(arr);
+	printf("%d\n", len); 
+
 	return 0;
 }
